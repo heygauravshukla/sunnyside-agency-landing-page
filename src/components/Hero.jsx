f2a33b5e375fd29c4744997557c8484a31114fe3
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-18 18:09:42 +0530

    Add basic markup to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,10 @@
+import arrow_down_icon from ".././assets/images/icon-arrow-down.svg";
+
 export default function Hero() {
-  return <section>Hero</section>;
+  return (
+    <section>
+      <h1>We are creatives</h1>
+      <img src={arrow_down_icon} alt="scroll below" width="36" height="114" />
+    </section>
+  );
 }
