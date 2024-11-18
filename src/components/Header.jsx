ecc7fc2985ac063d1f79b72794e35a8cc86dad6e
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-18 18:08:11 +0530

    Add basic markup to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,37 @@
+import logo_image from ".././assets/images/logo.svg";
+import hamburger_icon from ".././assets/images/icon-hamburger.svg";
+
+const links = [
+  { id: 1, name: "About", url: "/" },
+  { id: 2, name: "Services", url: "/" },
+  { id: 3, name: "Projects", url: "/" },
+];
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      {/* logo */}
+      <a href="/">
+        <img src={logo_image} alt="sunnyside logo" width="124" height="24" />
+      </a>
+      {/* menu toggler */}
+      <button>
+        <img src={hamburger_icon} alt="Menu toggler" width="24" height="18" />
+        <p>Menu</p>
+      </button>
+      {/* menu */}
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <a href={link.url}>{link.name}</a>
+            </li>
+          ))}
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
