6792eb0fb4aa33b2b01ed7ed14011814d60829e0
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-19 17:42:40 +0530

    Add "Gallery" component to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import DesignAndPhotography from "./components/DesignAndPhotography";
 import Testimonials from "./components/Testimonials";
+import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
       <Features />
       <DesignAndPhotography />
       <Testimonials />
+      <Gallery />
       <Footer />
     </>
   );
