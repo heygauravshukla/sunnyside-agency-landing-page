42e235317ee860d68b4afa968641d90c01332cb8
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-20 10:43:46 +0530

    Refactor: Update variable names to camelCase and remove height/width attributes from Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
-import arrow_down_icon from ".././assets/images/icon-arrow-down.svg";
+import arrowDownIcon from "../assets/images/icon-arrow-down.svg";
 
 export default function Hero() {
   return (
     <section>
       <h1>We are creatives</h1>
-      <img src={arrow_down_icon} alt="scroll below" width="36" height="114" />
+      <img src={arrowDownIcon} alt="Scroll below" />
     </section>
   );
 }
