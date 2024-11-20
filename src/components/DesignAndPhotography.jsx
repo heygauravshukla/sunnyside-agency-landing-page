a5197fd807cbfd4f550b818ebf4d86e2839a5986
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-20 13:34:43 +0530

    Refactor: removed unnecessary alt prop, utilized heading prop

--- src/components/DesignAndPhotography.jsx
+++ src/components/DesignAndPhotography.jsx
@@ -3,12 +3,12 @@ import graphicDesignDesktop from "../assets/images/desktop/image-graphic-design.
 import photographyMobile from "../assets/images/mobile/image-photography.jpg";
 import photographyDesktop from "../assets/images/desktop/image-photography.jpg";
 
-function Card({ mobileImage, desktopImage, alt, heading, paragraph }) {
+function Card({ mobileImage, desktopImage, heading, paragraph }) {
   return (
     <div>
       <picture>
         <source srcSet={desktopImage} media="(min-width: 600px)" />
-        <img src={mobileImage} alt={alt} />
+        <img src={mobileImage} alt={`${heading} image`} />
       </picture>
       <div>
         <h3>{heading}</h3>
@@ -24,14 +24,12 @@ export default function DesignAndPhotography() {
       <Card
         mobileImage={graphicDesignMobile}
         desktopImage={graphicDesignDesktop}
-        alt="Graphic design image"
         heading="Graphic design"
         paragraph="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention."
       />
       <Card
         mobileImage={photographyMobile}
         desktopImage={photographyDesktop}
-        alt="Photography design image"
         heading="Photography"
         paragraph="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
       />
