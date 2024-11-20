d676d581c47aefd479e8f1ecb6a213afa143a747
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-20 13:22:29 +0530

    Refactor: Rename 'title' to 'heading', 'description' to 'paragraph', 'url' to 'href', and 'altText' to 'alt'

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,21 +3,21 @@ import transformDesktop from "../assets/images/desktop/image-transform.jpg";
 import standoutMobile from "../assets/images/mobile/image-stand-out.jpg";
 import standoutDesktop from "../assets/images/desktop/image-stand-out.jpg";
 
-function TextBox({ title, description, url }) {
+function TextBox({ heading, paragraph, href }) {
   return (
     <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <a href={url}>Learn more</a>
+      <h2>{heading}</h2>
+      <p>{paragraph}</p>
+      <a href={href}>Learn more</a>
     </div>
   );
 }
 
-function ImageBox({ mobileImage, desktopImage, altText }) {
+function ImageBox({ mobileImage, desktopImage, alt }) {
   return (
     <picture>
       <source srcSet={desktopImage} media="(min-width: 600px)" />
-      <img src={mobileImage} alt={altText} />
+      <img src={mobileImage} alt={alt} />
     </picture>
   );
 }
@@ -26,28 +26,28 @@ export default function Features() {
   return (
     <section>
       <TextBox
-        title="Transform your brand"
-        description="We are a full-service creative agency specializing in helping brands
+        heading="Transform your brand"
+        paragraph="We are a full-service creative agency specializing in helping brands
           grow fast. Engage your clients through compelling visuals that do most
           of the marketing for you."
-        url="/"
+        href="/"
       />
       <ImageBox
         mobileImage={transformMobile}
         desktopImage={transformDesktop}
-        altText="Transform image"
+        alt="Transform image"
       />
       <ImageBox
         mobileImage={standoutMobile}
         desktopImage={standoutDesktop}
-        altText="Stand out image"
+        alt="Stand out image"
       />
       <TextBox
-        title="Stand out to the right audience"
-        description=" Using a collaborative formula of designers, researchers,
+        heading="Stand out to the right audience"
+        paragraph=" Using a collaborative formula of designers, researchers,
           photographers, videographers, and copywriters, we’ll build and extend
           your brand in digital places."
-        url="/"
+        href="/"
       />
     </section>
   );
