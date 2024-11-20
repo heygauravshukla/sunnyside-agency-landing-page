ebf8cfe7882cc1a77596dda16ea63dfb78bda779
Gaurav Shukla <heygauravshukla@gmail.com>
2024-11-20 17:24:25 +0530

    Added custom colors to the Tailwind config and index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "soft-red": "hsl(var(--color-soft-red))",
+      yellow: "hsl(var(--color-yellow))",
+      "dark-desaturated-cyan": "hsl(var(--color-dark-desaturated-cyan))",
+      "dark-blue": "hsl(var(--color-dark-blue))",
+      "dark-moderate-cyan": "hsl(var(--color-dark-moderate-cyan))",
+      "very-dark-desaturated-blue":
+        "hsl(var(--color-very-dark-desaturated-blue))",
+      "very-dark-grayish-blue": "hsl(var(--color-very-dark-grayish-blue))",
+      "dark-grayish-blue": "hsl(var(--color-dark-grayish-blue))",
+      "grayish-blue": "hsl(var(--color-grayish-blue))",
+      white: "hsl(var(--color-white))",
+    },
     extend: {},
   },
   plugins: [],
